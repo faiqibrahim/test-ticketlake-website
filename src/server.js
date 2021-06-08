ab1d45b76ca10b1bad69f9b2f43d2ecfe55814a3
Usman Zaheer <usman.zaheer@synavos.com>
2021-06-08 12:57:30 +0500

    added QA backend https URL and mattermost notifications

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const GET_URL = () => {
   switch (REACT_APP_ENV.trim()) {
     case "dev":
       //url = "http://192.168.2.50:5030/";
-      url = "http://192.168.2.49:5032/";
+      url = "https://api.qa.ticketlake.com/";
       break;
     case "qa":
-      url = "http://192.168.2.49:5032/";
+      url = "https://api.ticketlake.qa.com/";
       break;
     case "staging":
       url = "https://stagingapi.ticketlake.com/";
