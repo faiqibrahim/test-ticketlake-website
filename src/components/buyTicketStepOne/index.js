a85e0584de2064912e538c78faec5c961e5c3276
Usman Akmal <usman.akmal@synavos.com>
2021-07-30 01:43:01 +0500

    Buy Ticket Step One code refactored for sats view - displayed functional table

--- src/components/buyTicketStepOne/index.js
+++ src/components/buyTicketStepOne/index.js
@@ -3,7 +3,6 @@ import React from "react";
 import ClassQuanityTable from "./ClassQuantityTable";
 import { Radio, Form } from "antd";
 import "./style.css";
-import { seatFormJSON } from "../../utils/json-utils";
 
 const SeatsRadio = (props) => {
   const { value, onChange, radioOptions, name } = props;
@@ -27,75 +26,11 @@ class BuyTicketStepOne extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formJSON: [...this.getFormJSON()],
+      purchaseType: "",
+      seatSelection: "",
+      seatsType: "",
     };
   }
-
-  getFormJSON = () => {
-    const { ticketsAndPassesInfo } = this.props;
-    const { isTickets, isPasses } = ticketsAndPassesInfo;
-
-    const jsonProps = {
-      onTypeChange: this.onPurchaseTypeChange,
-      onSelectionChange: this.onSelectionChange,
-      isTickets,
-      isPasses,
-    };
-
-    return seatFormJSON(jsonProps);
-  };
-
-  //#region Event Handlers
-
-  onPurchaseTypeChange = (value) => {
-    this.setState({
-      purchaseType: value,
-      seatSelection: "",
-      formJSON: [...this.getFormJSON()],
-    });
-  };
-
-  onSelectionChange = (seatSelection) => {
-    const { purchaseType } = this.state;
-    const { name, value: selectionValue } = seatSelection;
-    const formJSON = [...this.getFormJSON()];
-
-    const seatTypeObject = {
-      label: "Seats Type",
-      name: "seatsType",
-      renderOnKey: "seatSelection",
-      type: "radio",
-      radioOptions: [
-        { value: "seat", name: "Seat" },
-        { value: "Table", name: "Table" },
-      ],
-      onChange: this.onSeatTypeChange,
-    };
-
-    const isAutoTicket = purchaseType === "ticket" && selectionValue === "auto";
-
-    if (purchaseType === "pass" || isAutoTicket) {
-      formJSON.push(seatTypeObject);
-    }
-
-    this.setState({ [name]: selectionValue, seatsType: "", formJSON });
-  };
-
-  onSeatTypeChange = ({ target }) => {
-    const formJSON = [...this.state.formJSON];
-
-    formJSON.length < 4 &&
-      formJSON.push({
-        name: "classTable",
-        label: "",
-        renderOnKey: "seatSelection",
-        customComponent: this.displayClassesTable(),
-      });
-
-    this.setState({ seatsType: target.value, formJSON });
-  };
-
-  //#endregion
 
   displayClassesTable = () => {
     const { isStandard, ticketClasses, passClasses } = this.props;
@@ -118,52 +53,98 @@ class BuyTicketStepOne extends React.Component {
     );
   };
 
-  renderSeatsView = () => {
-    const { formJSON } = this.state;
-
-    const animatedStyle = (key) => {
-      const value = this.state[key];
-      return {
-        height: value ? "auto" : "0",
-        marginBottom: value ? "24px" : "0",
-        opacity: value ? "1" : "0",
-        transition: "opacity 1s linear ",
-      };
+  animatedStyle = (key) => {
+    const value = this.state[key];
+    return {
+      height: value ? "auto" : "0",
+      marginBottom: value ? "24px" : "0",
+      opacity: value ? "1" : "0",
+      transition: "opacity 1s linear ",
     };
+  };
+
+  displayPurchaseType = () => {
+    const { ticketsAndPassesInfo } = this.props;
+    const { isTickets, isPasses } = ticketsAndPassesInfo;
+    const typeOptions = [];
+    isTickets && typeOptions.push({ value: "ticket", name: "Ticket" });
+    isPasses && typeOptions.push({ value: "pass", name: "Pass" });
 
     return (
-      <div className="col-md-12 text-left mb-5 seatsView">
-        {formJSON.map((jsonItem, index) => {
-          const {
-            renderOnKey,
-            name,
-            label,
-            type,
-            customComponent,
-            radioOptions,
-            onChange,
-          } = jsonItem;
+      <Form.Item label={"Select Purchase Type"}>
+        <SeatsRadio
+          name={"purchaseType"}
+          value={this.state["purchaseType"]}
+          radioOptions={typeOptions}
+          onChange={({ target }) =>
+            this.setState({
+              purchaseType: target.value,
+              seatSelection: "",
+            })
+          }
+        />
+      </Form.Item>
+    );
+  };
 
-          return (
-            <React.Fragment key={index}>
-              {type === "radio" ? (
-                <Form.Item
-                  label={label}
-                  style={renderOnKey && animatedStyle(renderOnKey)}
-                >
-                  <SeatsRadio
-                    name={name}
-                    value={this.state[name]}
-                    radioOptions={radioOptions}
-                    onChange={onChange}
-                  />
-                </Form.Item>
-              ) : (
-                customComponent
-              )}
-            </React.Fragment>
-          );
-        })}
+  displaySeatSelection = () => {
+    return (
+      <Form.Item
+        label="Seats Selection"
+        style={this.animatedStyle("purchaseType")}
+      >
+        <SeatsRadio
+          name={"seatSelection"}
+          value={this.state["seatSelection"]}
+          radioOptions={[
+            { value: "preferred", name: "Preferred" },
+            { value: "auto", name: "Auto" },
+          ]}
+          onChange={({ target }) =>
+            this.setState({ seatSelection: target.value, seatsType: "" })
+          }
+        />
+      </Form.Item>
+    );
+  };
+
+  displaySeatType = () => {
+    const { purchaseType, seatSelection } = this.state;
+
+    if (purchaseType === "ticket" && seatSelection === "preferred") return null;
+
+    return (
+      <Form.Item label="Seats Type" style={this.animatedStyle("seatSelection")}>
+        <SeatsRadio
+          name={"seatsType"}
+          value={this.state["seatsType"]}
+          radioOptions={[
+            { value: "seat", name: "Seat" },
+            { value: "Table", name: "Table" },
+          ]}
+          onChange={({ target }) => this.setState({ seatsType: target.value })}
+        />
+      </Form.Item>
+    );
+  };
+
+  displayClasses = () => {
+    return (
+      <div style={this.animatedStyle("seatsType")}>
+        {this.displayClassesTable()}
+      </div>
+    );
+  };
+
+  renderSeatsView = () => {
+    return (
+      <div className="col-md-12 text-left mb-5 seatsView">
+        <React.Fragment>
+          {this.displayPurchaseType()}
+          {this.displaySeatSelection()}
+          {this.displaySeatType()}
+          {this.displayClasses()}
+        </React.Fragment>
       </div>
     );
   };
