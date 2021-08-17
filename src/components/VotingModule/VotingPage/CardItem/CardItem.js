21c636fe67cf418a43bfb540f16f5b001e62ddd6
harf <hafiz.abdulrehman@synavos.com>
2021-08-17 15:23:23 +0500

    winner badge adjustment

--- src/components/VotingModule/VotingPage/CardItem/CardItem.js
+++ src/components/VotingModule/VotingPage/CardItem/CardItem.js
@@ -6,8 +6,10 @@ import { duration } from "../Duration/duration";
 import "./CardItem.css";
 
 const CardItem = (props) => {
+  const { name, status, clicked, votingCounting, images } = props;
+
   let closedVoting = null;
-  if (props.status) {
+  if (status) {
     closedVoting = (
       <>
         <img src={"/images/votingimages/winner.svg"} alt="img" />
@@ -16,25 +18,25 @@ const CardItem = (props) => {
     );
   }
 
-  let eventName =
-    props.name.length > 20 ? (
+  const eventName =
+    name.length > 21 ? (
       <>
         {" "}
-        <div className="cardTitle" data-tip={props.name}>
-          {props.name}
+        <div className="cardTitle" data-tip={name}>
+          {name}
         </div>
         <ReactTooltip place="right" className={"tooltipStyle"} />
       </>
     ) : (
-      <div className="cardTitle">{props.name}</div>
+      <div className="cardTitle">{name}</div>
     );
 
   return (
-    <div className="cardItemCol" onClick={props.clicked}>
+    <div className="cardItemCol" onClick={clicked}>
       <div className="imageContainer">
-        <div className={props.status ? "overlay" : ""}></div>
+        <div className={status ? "overlay" : ""}></div>
         <div className="closedVoting">{closedVoting}</div>
-        <img src={props.images} alt={"img"} />
+        <img src={images} alt={"img"} />
       </div>
       <div className="cardContent">
         {eventName}
@@ -44,7 +46,7 @@ const CardItem = (props) => {
               ? duration({ ...props }).durationString
               : duration({ ...props })}
           </div>
-          <div className="voteCount">{props.votingCounting} Votes</div>
+          <div className="voteCount">{votingCounting} Votes</div>
         </div>
       </div>
     </div>
