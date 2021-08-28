e199750f858eaa5a93495a3872ed13796d4d100f
Usman Akmal <usman.akmal@synavos.com>
2021-08-28 18:05:58 +0500

    Versioning done for API's

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,23 +1,27 @@
-import axios from 'axios';
-import {SERVER_URL} from './config';
-import store from '../redux/store';
+import axios from "axios";
+import { SERVER_URL } from "./config";
+import store from "../redux/store";
 
 const getOptions = () => {
-    return {
-        headers: {
-            'X-Auth': store.getState().user.token
-        }
-    };
+  return {
+    headers: {
+      "X-Auth": store.getState().user.token,
+      "x-version": 2.5,
+    },
+  };
 };
 
-
-const prepareUrl = (api, version) => (SERVER_URL+version + api);
+const prepareUrl = (api, version) => SERVER_URL + version + api;
 
 const wrapper = {
-    get: (api,  version= 'v1') => axios.get(prepareUrl(api, version), getOptions()),
-    post: (api, formData = {},  version= 'v1') => axios.post(prepareUrl(api, version), formData, getOptions()),
-    put: (api, formData = {},  version= 'v1') => axios.put(prepareUrl(api, version), formData, getOptions()),
-    delete: (api,  version= 'v1') => axios.delete(prepareUrl(api, version), getOptions()),
+  get: (api, version = "v1") =>
+    axios.get(prepareUrl(api, version), getOptions()),
+  post: (api, formData = {}, version = "v1") =>
+    axios.post(prepareUrl(api, version), formData, getOptions()),
+  put: (api, formData = {}, version = "v1") =>
+    axios.put(prepareUrl(api, version), formData, getOptions()),
+  delete: (api, version = "v1") =>
+    axios.delete(prepareUrl(api, version), getOptions()),
 };
 
 export default wrapper;
