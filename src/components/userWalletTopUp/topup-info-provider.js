28a9eff6acca13c6145958cdd9f0444d56157c40
Usman Akmal <usman.akmal@synavos.com>
2021-09-01 19:24:49 +0500

    Wallet option removed from topup info

--- src/components/userWalletTopUp/topup-info-provider.js
+++ src/components/userWalletTopUp/topup-info-provider.js
@@ -1,28 +1,27 @@
-import {getStore} from '../../index';
-import {formatCurrency} from "../../utils/common-utils";
+import { getStore } from "../../index";
+import { formatCurrency } from "../../utils/common-utils";
 
 export const getTopUpInfo = () => {
-    const state = getStore().getState();
-    const {walletTopUp} = state.user;
+  const state = getStore().getState();
+  const { walletTopUp } = state.user;
 
-    return {
-        amount: walletTopUp.topUpAmount,
-        currency: walletTopUp.walletCurrency,
-        purpose: 'WALLET_TOP_UP',
-        description: `${formatCurrency(walletTopUp.topUpAmount, walletTopUp.walletCurrency)} Wallet Topup`,
-        paymentMethods: [
-            {
-                "type": "WALLET",
-                "walletCurrency": state.user.userWallet.currency,
-                "balance": state.user.userWallet.availableBalance,
-            },
-            {
-                "type": "MOBILE_MONEY",
-            },
-            {
-                "type": "PAYPAL",
-                "clientId": "AVxMDtg2UkfX0IFBK86r_l_EcCeloAcMmOQf7vbOuPQsr10I5QJBf-u4YVn504puI-GyLQ0ZcKRYBG2T"
-            }
-        ]
-    }
-}
+  return {
+    amount: walletTopUp.topUpAmount,
+    currency: walletTopUp.walletCurrency,
+    purpose: "WALLET_TOP_UP",
+    description: `${formatCurrency(
+      walletTopUp.topUpAmount,
+      walletTopUp.walletCurrency
+    )} Wallet Topup`,
+    paymentMethods: [
+      {
+        type: "MOBILE_MONEY",
+      },
+      {
+        type: "PAYPAL",
+        clientId:
+          "AVxMDtg2UkfX0IFBK86r_l_EcCeloAcMmOQf7vbOuPQsr10I5QJBf-u4YVn504puI-GyLQ0ZcKRYBG2T",
+      },
+    ],
+  };
+};
